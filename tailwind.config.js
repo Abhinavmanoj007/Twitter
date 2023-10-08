efc489a38e1202f4f2d537eb8f4ec3d14c355723
Abhinav Manoj <abhinavmanoj99@gmail.com>
2023-10-08 02:16:18 -0700

    firstCommit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      poppin: ["Poppins", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+    },
+    colors: {
+      blue: "#1DA1F2",
+      black: "#14171A",
+      darkGray: "#657786",
+      lightGray: "#AAB8C2",
+      xlightGray: "#E1E8ED",
+      xxlightGray: "#F5F8FA",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
